ac6d41f0613e859eda0da879899ee4dc84f9e3ca
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-19 09:43:39 -0800

    creating backend

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const sequelize = require("./config/connection.js");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const routes = require("./controllers");
+
+// Set up react for the app's front end
+
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routing utility 
+app.use(routes)
+
+//Starting the server
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+    });
+});
