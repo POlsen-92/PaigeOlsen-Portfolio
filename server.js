256f58c91b008cde8a943cb97c6e4d7e163afddd
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-22 22:03:53 -0800

    adding different file structure

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
 //routing utility 
 app.use(routes)
 
