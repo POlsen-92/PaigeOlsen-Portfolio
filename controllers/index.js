22640ea1fdca7733d7a7d16fc9bdfab7cfef979f
FemBot 9000 <paige.el.olsen@gmail.com>
2021-12-09 09:39:02 -0800

    turned into front end only

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const frontEndRoutes = require("./frontEndRoutes.js");
-router.use(frontEndRoutes);
-
-module.exports = router;
